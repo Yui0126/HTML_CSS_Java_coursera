ce79730e2cf83cd51f348b1902a27d6f0e8961e2
Yui0126 <85067724+Yui0126@users.noreply.github.com>
2022-06-30 21:25:23 +0900

    commit

--- module4_test/js/script.js
+++ module4_test/js/script.js
@@ -448,3 +448,14 @@ johnGreeter.sayHi();
 (function(name) {
     console.log("Hello " + name);
 })("Coursera!");
+
+$('.button').click(function() {
+    var buttonId = $(this).attr('id');
+    $('#modal-container').removeAttr('class').addClass(buttonId);
+    $('body').addClass('modal-active');
+})
+
+$('#modal-container').click(function() {
+    $(this).addClass('out');
+    $('body').removeClass('modal-active');
+});
